16f5504a6c18a86cf3a5b2c677a3ab87be773b11
Zachary Swensrude <swensrudez@gmail.com>
2024-01-19 12:53:13 -0700

    update index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const add = require('./add.js');
 const findNaNIndex = require('./findNaNIndex.js')
+const isArrayFactorChain = require('./isArrayFactorChain.js')
 
 module.exports = {
   add,
-  findNaNIndex
+  findNaNIndex,
+  isArrayFactorChain
 };
 
 
